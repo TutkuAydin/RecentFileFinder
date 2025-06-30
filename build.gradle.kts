3d712d7da0e405b70c382b0021103b50c3f210c5
Tutku Aydın <taydin@protel.com.tr>
2025-06-30 09:17:50 +0300

    Change version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.jetbrains.intellij") version "1.17.3"
 }
 
-group = "com.example"
-version = "1.0-SNAPSHOT"
+group = "com.tutkuaydin"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
